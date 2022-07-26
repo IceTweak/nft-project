0d0795995ef578414132358109f6b2e1780d70a6
Roman Savoshchik <tweakice@gmail.com>
2022-07-26 16:07:44 +0600

    Override baseURI

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -4,21 +4,25 @@ pragma solidity 0.8.15;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+/// @title ERC721 Token implementation
+contract MyNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor() ERC721("MyNFT", "NFT") {}
 
-    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+    function mintNFT(address recipient) public onlyOwner returns (uint256) {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
+    }
+
+    /// @dev Changes baseURI to ipfs directory with tokens properties
+    function _baseURI() internal view virtual override returns (string memory) {
+        return "ipfs//linkToMyBase";
     }
 }
