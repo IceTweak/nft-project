3d9fdb56dc4f50613804549ad4b4eb76f41e4da7
Roman Savoshchik <tweakice@gmail.com>
2022-07-28 15:12:05 +0600

    Add override for tokenURI function

--- contracts/StarWarsNft.sol
+++ contracts/StarWarsNft.sol
@@ -3,14 +3,16 @@ pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title ERC721 Token implementation
 contract StarWarsNft is ERC721, Ownable {
     using Counters for Counters.Counter;
+    using Strings for uint256;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Star Wars NFT", "SW") {}
+    constructor() ERC721("Marvel Star Wars", "MSW") {}
 
     function mintNFT(address recipient) public onlyOwner returns (uint256) {
         _tokenIds.increment();
@@ -21,8 +23,16 @@ contract StarWarsNft is ERC721, Ownable {
         return newItemId;
     }
 
-    /// @dev Changes baseURI to ipfs directory with tokens properties
+    /// @dev Returns URI for each of minted token
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        _requireMinted(tokenId);
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : "";
+    }
+
+    /// @dev Returns baseURI to ipfs directory with tokens properties
     function _baseURI() internal view virtual override returns (string memory) {
-        return "https://gateway.pinata.cloud/ipfs/QmWimriCiyQy6kNmLswiPd7odjDmB5EsAeyCTqhsE7dpmd/";
+        return "https://gateway.pinata.cloud/ipfs/QmbtHKYt6HfDnneEwBwWPxzip37ULNb5qop9gAdxySwZZr/";
     }
 }
